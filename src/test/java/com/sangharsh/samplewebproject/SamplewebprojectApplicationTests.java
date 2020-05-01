8f04d99d5cd02360ada54dd22a801972fedd937f
Sangharsh <sangharsh.khaire@yahoo.com>
2020-05-01 19:08:19 +0530

    first commit for Sample Web Project

---
+++ src/test/java/com/sangharsh/samplewebproject/SamplewebprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangharsh.samplewebproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplewebprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
